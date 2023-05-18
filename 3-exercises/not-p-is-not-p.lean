c7df461ed145d298098ba3f6a0528b1932d0ed88
kokushkin <>
2023-05-18 16:58:44 +0300

    continue to prove ∀ p: Prop, p ↔ ¬ p

--- 3-exercises/not-p-is-not-p.lean
+++ 3-exercises/not-p-is-not-p.lean
@@ -32,10 +32,20 @@ example: (p ↔ q) → (q ↔ p) :=
 example : ¬ ∀ p, p ↔ ¬ p := sorry
 
 
-def NegNotEq {p: Prop} (h: ∀ p, p ↔ ¬ p):  false := 
+variable x:  (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p)
+#check  x
+#check iff.mp x
+
+variable h1:  ∀ p: Prop, p ↔ ¬ p
+#check h1
+variable p1: Prop
+#check h1 p1
+#check h1 
+
+def NegNotEq  (h: ∀ p: Prop, p ↔ ¬ p):  false := 
 assume p: Prop, 
 have pisnotp: p ↔ ¬ p, from h p,
-have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h pisnotp,
--- have notPisnotp: ¬(p → ¬ p), from iff.mp (h ()
+have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
+have notPisnotp: ¬(p → ¬ p), from iff.mp ((p ↔ ¬ p) ↔ ¬(p ↔ ¬ p)) (p ↔ ¬ p),
 
 #check  NegNotEq
