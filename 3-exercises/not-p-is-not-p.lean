c71631a95f4a0f3acf053554402827e74cbc8c7f
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2023-08-28 11:16:36 +0300

    add a different proof of a different NegNotEq

--- 3-exercises/not-p-is-not-p.lean
+++ 3-exercises/not-p-is-not-p.lean
@@ -1,47 +1,76 @@
-variables p q r : Prop
-
--- Prove ¬(p ↔ ¬p) without using classical logic.
--- p ↔ ¬p - > false  
-
-example: ¬(p ↔ ¬p) :=-
-assume pisnotp: p ↔ ¬p,
-iff.mp p ¬p pisnotp
+variable r: Prop
 
 
+-- it's not true for all of them
+def NegNotEq  (h: (∀ p: Prop, p ↔ ¬ p)):  false :=
 
-
-example: ¬(p ↔ ¬p) :=
-assume pisnotp: p ↔ ¬p,
-have h1: p → ¬p  , from iff.mp pisnotp,
-have h2: ¬ (p ↔ ¬p), from h1 pisnotp,
-
-
-
-example: (p ↔ q) → (q ↔ p) :=
-  assume pq: p ↔ q,
-   iff.intro
-   (assume hq: q,
-    iff.mpr pq hq)
-   (assume hp: p,
-    iff.mp pq hp)
-   
-
-
--- We're proving that ¬(p ↔ ¬ p) is true for every p: Prop is true, right?
-
-example : ¬ ∀ p, p ↔ ¬ p := sorry
-
-
-
-def NegNotEq  (h: (∀ p: Prop, p ↔ ¬ p)):  false := 
 have anypfalse: Prop → false, from
+
 (assume p: Prop, 
 have pisnotp: p ↔ ¬ p, from h p,
 have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
 have notPisnotp: (p ↔ ¬ p) → ¬(p ↔ ¬ p), from iff.mp pisnotpeq,
 have notPISNOTP: ¬(p ↔ ¬ p), from notPisnotp pisnotp,
  notPISNOTP pisnotp),
- anypfalse true
+
+ anypfalse r
+
+
+-- Prove ¬(p ↔ ¬p) without using classical logic
+-- it's false for all of them
+def NegNotEq2 (h: (∀ p: Prop, ¬(p ↔ ¬p))): true :=
+    assume q: Prop,
+    assume h : q ↔ ¬q,
+    iff.elim
+        (
+            assume hl : q → ¬q,
+            assume hr : ¬q → q,
+            have hnp : ¬q,
+            from
+                assume hp : q,
+                hl hp hp,
+            hnp (hr hnp)
+        ) h
+
+
+--     --
+variable p: Prop
+
+def asdasd (hl : p → ¬p, hr : ¬p → p): false :=
+(hl hp) hp
+
+
+
+
+example : ¬(p ↔ ¬p) :=
+    assume h : p ↔ ¬p,
+    iff.elim
+        (
+            assume hl : p → ¬p,
+            assume hr : ¬p → p,
+            have hnp : ¬p,
+            from
+                assume hp : p,
+                hl hp hp,
+            hnp (hr hnp)
+        ) h
+
+
+
+
+--  def NegNotEq  (h: (∀ p: Prop, p ↔ ¬ p)):  false :=
+-- assume p: Prop, 
+-- have pisnotp: p ↔ ¬ p, from h p,
+-- have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
+-- have notPisnotp: (p ↔ ¬ p) → ¬(p ↔ ¬ p), from iff.mp pisnotpeq,
+-- have notPISNOTP: ¬(p ↔ ¬ p), from notPisnotp pisnotp,
+--  notPISNOTP pisnotp
+
+def add: Int → Int → Int :=
+    assume a, assume b, a + b
+
+
+
 
 
 #check  NegNotEq
