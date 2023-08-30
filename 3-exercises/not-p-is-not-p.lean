9a10ac2d90a7a9405be2ee55529fe5e9693c43d7
kokushkin <>
2023-08-30 16:09:22 +0300

    make both NotTrueForAll and FalseForAll proofs' types easy to compare

--- 3-exercises/not-p-is-not-p.lean
+++ 3-exercises/not-p-is-not-p.lean
@@ -1,48 +1,27 @@
-variable r: Prop
+-- Prove ¬(p ↔ ¬p) without using classical logic
 
 
--- it's not true for all of them
-def NegNotEq  (h: (∀ p: Prop, p ↔ ¬ p)):  false :=
-
+-- it's not true for all of them (this is not what they meant but still interesting proof)
+ def NotTrueForAll : ¬ (∀ p: Prop, p ↔ ¬ p) :=
+assume h: (∀ p: Prop, p ↔ ¬ p),
 have anypfalse: Prop → false, from
 
 (assume p: Prop, 
-have pisnotp: p ↔ ¬ p, from h p,
-have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
-have notPisnotp: (p ↔ ¬ p) → ¬(p ↔ ¬ p), from iff.mp pisnotpeq,
-have notPISNOTP: ¬(p ↔ ¬ p), from notPisnotp pisnotp,
+  have pisnotp: p ↔ ¬ p, from h p,
+  have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
+  have notPisnotp: (p ↔ ¬ p) → ¬(p ↔ ¬ p), from iff.mp pisnotpeq,
+  have notPISNOTP: ¬(p ↔ ¬ p), from notPisnotp pisnotp,
  notPISNOTP pisnotp),
 
- anypfalse r
+-- instead of true we could substitute any prooven proposition
+ anypfalse true
 
 
--- Prove ¬(p ↔ ¬p) without using classical logic
 -- it's false for all of them
-def NegNotEq2 (h: (∀ p: Prop, ¬(p ↔ ¬p))): true :=
-    assume q: Prop,
-    assume h : q ↔ ¬q,
-    iff.elim
-        (
-            assume hl : q → ¬q,
-            assume hr : ¬q → q,
-            have hnp : ¬q,
-            from
-                assume hp : q,
-                hl hp hp,
-            hnp (hr hnp)
-        ) h
-
-
---     --
-variable p: Prop
-
-def asdasd (hl : p → ¬p, hr : ¬p → p): false :=
-(hl hp) hp
-
-
-
-
-example : ¬(p ↔ ¬p) :=
+def FalseForAll: (∀ p: Prop, ¬(p ↔ ¬p)) :=
+assume p: Prop,
+show ¬(p ↔ ¬p), from
+(
     assume h : p ↔ ¬p,
     iff.elim
         (
@@ -54,24 +33,4 @@ example : ¬(p ↔ ¬p) :=
                 hl hp hp,
             hnp (hr hnp)
         ) h
-
-
-
-
---  def NegNotEq  (h: (∀ p: Prop, p ↔ ¬ p)):  false :=
--- assume p: Prop, 
--- have pisnotp: p ↔ ¬ p, from h p,
--- have pisnotpeq: (p ↔ ¬ p) ↔ ¬(p ↔ ¬ p), from h (p ↔ ¬ p),
--- have notPisnotp: (p ↔ ¬ p) → ¬(p ↔ ¬ p), from iff.mp pisnotpeq,
--- have notPISNOTP: ¬(p ↔ ¬ p), from notPisnotp pisnotp,
---  notPISNOTP pisnotp
--- asdasd
-
-def add: Int → Int → Int :=
-    assume a, assume b, a + b
-
-
-
-
-
-#check  NegNotEq
+)
